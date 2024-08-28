a5dd386e0c6f7bdc3ed40f35f6acac77da47a647
sergiobarria <sbarria.dev@gmail.com>
2024-08-28 15:04:47 -0500

    project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import inter from '@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url';
+	import interItalic from '@fontsource-variable/inter/files/inter-latin-wght-italic.woff2?url';
+
+	import '../app.css';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<svelte:head>
+	<!-- Preload fonts -->
+	<link rel="preload" as="font" type="font/woff2" href={inter} crossorigin="anonymous" />
+	<link rel="preload" as="font" type="font/woff2" href={interItalic} crossorigin="anonymous" />
+
+	<title>Outdoors Explorers</title>
+</svelte:head>
+
+{@render children()}
