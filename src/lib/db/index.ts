39436886bfb6f1d019e19694f88b944501c59604
sergiobarria <sbarria.dev@gmail.com>
2024-08-28 21:50:16 -0500

    remove old project

--- src/lib/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-
-import { DATABASE_URL, DATABASE_AUTH_TOKEN } from '$env/static/private';
-import * as schema from './schema';
-
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
-export const db = drizzle(client, { schema });
