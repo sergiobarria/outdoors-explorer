6b89e64284354cbb4d9bc4b4847442112b83cc0a
sergiobarria <sbarria.dev@gmail.com>
2024-08-29 15:04:38 -0500

    display tours list

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 export { default as heroImg } from './hero-1.webp';
 export { default as aboutImg1 } from './about-1.webp';
 export { default as aboutImg2 } from './about-2.webp';
+export { default as communityImg } from './community.webp';
 export { default as placeholderImg } from './placeholder-image.jpg';
---
+++ src/db/types.ts
@@ -0,0 +1,9 @@
+import * as schemas from './schema';
+
+export type Tour = typeof schemas.toursTable.$inferSelect;
+export type StartDate = typeof schemas.startDatesTable.$inferSelect;
+export type TourImage = typeof schemas.imagesTable.$inferSelect;
+export type TourDetail = Omit<Tour, 'created' | 'modified' | 'description'> & {
+	images: string[];
+	startDates: string[];
+};
