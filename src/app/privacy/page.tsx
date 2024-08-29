8fe3d03a676fdfc077ee4c2a2cad222da661ad60
sergiobarria <sbarria.dev@gmail.com>
2024-08-29 02:18:19 -0500

    remove old

--- src/app/privacy/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPage() {
-	return (
-		<div>
-			<h1>Privacy</h1>
-			<p>This is the privacy page.</p>
-		</div>
-	);
-}
--- src/db/types.ts
+++
@@ -1,9 +0,0 @@
-import * as schemas from './schema';
-
-export type Tour = typeof schemas.toursTable.$inferSelect;
-export type TourImage = typeof schemas.imagesTable.$inferSelect;
-export type StartDate = typeof schemas.startDatesTable.$inferSelect;
-export type TourList = Omit<Tour, 'created' | 'modified' | 'description'> & {
-	images: string[];
-	startDates: string[];
-};
