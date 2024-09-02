981052d939901935bca5c6d80b63bf2b2a09c114
sergiobarria <sbarria.dev@gmail.com>
2024-09-01 20:54:50 -0500

    refactor and tours details page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,10 +17,34 @@ export function calculateDiscountPrice(price: number, discount: number | null) {
 	return price - (price * discount) / 100;
 }
 
-export function formatDate(date: string) {
+export function formatDate(date?: string) {
+	if (!date) return '';
 	return new Date(date).toLocaleDateString('en-US', {
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric'
 	});
+}
+
+/**
+ * Given a date string, return a formatted date and time string
+ * @param date - Date string
+ * @returns Formatted date and time string (e.g. 'Jan 1, 2021 at 12:00 PM')
+ */
+export function formatDateTime(dateString?: string) {
+	if (!dateString) return '';
+
+	const date = new Date(dateString);
+
+	const options: Intl.DateTimeFormatOptions = {
+		month: 'short',
+		day: '2-digit',
+		year: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+		timeZone: 'UTC' // Assuming the time should be in UTC
+	};
+
+	return date.toLocaleString('en-US', options).replace(',', ' at');
 }
