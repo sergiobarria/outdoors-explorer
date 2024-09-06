685fff22d2be96751cf576c438b126765a632fbf
sergiobarria <sbarria.dev@gmail.com>
2024-09-05 19:19:10 -0500

    remove drizzle orm

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-
-import * as schema from './schema';
-
-const { DATABASE_URL, DATABASE_AUTH_TOKEN } = import.meta.env;
-
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
-export const db = drizzle(client, { schema });
